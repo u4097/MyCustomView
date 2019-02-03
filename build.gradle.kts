891a5476f63d532f0e51b73af8cf7e772b943505
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-02-03 11:53:32 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.gradle.build-scan") version "2.1"
+}
+
+buildScan {
+    termsOfServiceUrl = "https://gradle.com/terms-of-service"
+    termsOfServiceAgree = "yes"
+    publishAlways()
+}
+
+buildscript {
+    repositories {
+        google()
+        jcenter()
+        
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.5.0-alpha03")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.20")
+        // NOTE: Do not place your application dependencies here; they belong
+        // in the individual module build.gradle files
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+        
+    }
+}
+
+tasks.register("clean",Delete::class.java) {
+    delete(rootProject.buildDir)
+}
