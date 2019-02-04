48a7ed5d36d7fbcd7f84f5eced3a5c9643e8577c
Oleg Sitnikov <o.sitnikov@pr-solution.ru>
2019-02-04 16:49:07 +0300

    add view pager adapter

--- app/src/main/java/com/olegsv/mycustomview/MyCustomDialogFragment.kt
+++ app/src/main/java/com/olegsv/mycustomview/MyCustomDialogFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import kotlinx.android.synthetic.main.fragment_my_custom_dialog.*
 
 /**
@@ -13,16 +16,37 @@ import kotlinx.android.synthetic.main.fragment_my_custom_dialog.*
 
 class MyCustomDialogFragment : DialogFragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
-        return inflater.inflate(R.layout.fragment_my_custom_dialog, container, false)
-    }
+		return inflater.inflate(R.layout.fragment_my_custom_dialog, container, false)
+	}
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 
-        ok_btn.setOnClickListener {
-            this.dismiss()
-        }
 
-    }
+	}
+}
+
+class ViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+	val mFragmentList = mutableListOf<Fragment>()
+	val mFragmentTitleList = mutableListOf<String>()
+
+
+	override fun getItem(position: Int): Fragment {
+		return mFragmentList[position]
+	}
+
+	override fun getCount(): Int {
+		return mFragmentList.size
+	}
+
+	fun addFragment(fragment: Fragment) {
+		mFragmentList.add(fragment)
+	}
+
+	override fun getPageTitle(position: Int): CharSequence? {
+		return mFragmentTitleList[position]
+	}
+
 }
